2f87aa9e29f6a4c74d3c89da33a44992bca733fa
Ivan Veselovskii <ivan.veselovskii@syncron.com>
2024-03-26 15:36:35 +0100

    added example with circular references in final classes.

---
+++ src/main/java/edu/TwoObjects.java
@@ -0,0 +1,50 @@
+package edu;
+
+import lombok.Getter;
+
+import java.util.function.Function;
+import java.util.function.IntSupplier;
+
+public class TwoObjects {
+
+    static class CircularRef<S> {
+        @Getter
+        private final S ref;
+        CircularRef(Function<CircularRef<S>, S> creator) {
+            ref = creator.apply(this);
+        }
+    }
+
+    static class CircularRefWithPayload extends CircularRef<CircularRefWithPayload> {
+        private final int v;
+        CircularRefWithPayload(Function<CircularRef<CircularRefWithPayload>, CircularRefWithPayload> creator, IntSupplier v1) {
+            super(creator);
+            v = v1.getAsInt();
+        }
+    }
+
+    public static void main(String[] args) {
+        CircularRef<CircularRef> circularRef1 = new CircularRef<>(x -> new CircularRef<>(y -> x));
+        System.out.println("r1 = " + circularRef1);
+        System.out.println("r1 -> ref = " + circularRef1.getRef());
+        System.out.println("r1 -> ref -> ref = " + circularRef1.getRef().getRef());
+
+
+        CircularRef<CircularRef<CircularRef>> circularRef3 = new CircularRef<>(
+                x -> new CircularRef<>(
+                        y -> new CircularRef<>(
+                                z -> x)));
+        System.out.println("r3 = " + circularRef3);
+        System.out.println("r3 -> ref = " + circularRef3.getRef());
+        System.out.println("r3 -> ref -> ref = " + circularRef3.getRef().getRef());
+        System.out.println("r3 -> ref -> ref -> ref = " + circularRef3.getRef().getRef().getRef());
+
+
+        CircularRefWithPayload p1 = new CircularRefWithPayload(
+                p -> new CircularRefWithPayload(q -> (CircularRefWithPayload)p, () -> 2),
+                () -> 1);
+        System.out.println("p1 = " + p1.v);
+        System.out.println("p1 -> ref = " + p1.getRef().v);
+        System.out.println("p1 -> ref -> ref = " + p1.getRef().getRef().v);
+    }
+}
