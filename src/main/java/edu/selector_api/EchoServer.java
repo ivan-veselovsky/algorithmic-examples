bc6ab1af315fc9400492cbe36b62c2bb37a8fd48
Ivan Veselovskii <ivan.veselovskii@syncron.com>
2024-05-03 17:30:14 +0200

    multithreaded selected keys processing,+ one more sample .

--- src/main/java/edu/selector_api/EchoServer.java
+++ src/main/java/edu/selector_api/EchoServer.java
@@ -9,8 +9,11 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.util.Iterator;
+import java.util.Collection;
+import java.util.ArrayList;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class EchoServer implements AutoCloseable {
@@ -21,7 +24,7 @@ public class EchoServer implements AutoCloseable {
     private Selector selector;
     private ServerSocketChannel serverSocketChannel;
     private final AtomicBoolean stop = new AtomicBoolean();
-    private final AtomicBoolean terminated = new AtomicBoolean();
+    private final CountDownLatch terminatedLatch = new CountDownLatch(1);
 
     @SneakyThrows
     public static void main(String[] args)  {
@@ -40,29 +43,38 @@ public class EchoServer implements AutoCloseable {
 
             startedCallback.run();
 
-            final ByteBuffer buffer = ByteBuffer.allocate(256);
+            final boolean synchronousMode = false;
             while (!stop.get()) {
                 int selected = selector.select();
-                //System.out.println("selected: " + selected);
-                Set<SelectionKey> selectedKeys = selector.selectedKeys();
-                Iterator<SelectionKey> it = selectedKeys.iterator();
-                while (it.hasNext()) {
-                    SelectionKey selectionKey = it.next();
-
+                final Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Collection<CompletableFuture<?>> futures = new ArrayList<>(selectedKeys.size());
+                for (SelectionKey selectionKey: selector.selectedKeys()) {
                     if (selectionKey.isAcceptable()) {
                         acceptAndRegister();
                     }
                     if (selectionKey.isReadable()) {
-                        if (!answerWithEcho(buffer, selectionKey)) {
-                            stop.compareAndSet(false, true);
+                        if (synchronousMode) {
+                            processRequest(selectionKey);
+                        } else {
+                            CompletableFuture<Void> f = CompletableFuture.runAsync(() -> processRequest(selectionKey));
+                            futures.add(f);
                         }
                     }
-
-                    it.remove();
                 }
+                if (!synchronousMode) {
+                    CompletableFuture.allOf(futures.toArray(CompletableFuture[]::new)).get();
+                }
+                selectedKeys.clear();
             }
         } finally {
-            terminated.compareAndSet(false, true);
+            terminatedLatch.countDown();
+        }
+    }
+
+    private void processRequest(SelectionKey selectionKey) {
+        ByteBuffer buffer = ByteBuffer.allocate(256);
+        if (!answerWithEcho(buffer, selectionKey)) {
+            stop.compareAndSet(false, true);
         }
     }
 
@@ -75,9 +87,7 @@ public class EchoServer implements AutoCloseable {
             selector0.wakeup();
         }
 
-        do {
-            Thread.sleep(1);
-        } while (!terminated.get());
+        terminatedLatch.await();
     }
 
     @Override
@@ -100,6 +110,9 @@ public class EchoServer implements AutoCloseable {
         SocketChannel socketChannel = (SocketChannel)selectionKey.channel();
         buffer.clear();
         int numberOfBytesRead = socketChannel.read(buffer);
+
+        Thread.sleep(1); // TODO: here we simulate some computational load
+
         if (numberOfBytesRead > 0) {
             buffer.flip();
             String message = readAsString(buffer);
