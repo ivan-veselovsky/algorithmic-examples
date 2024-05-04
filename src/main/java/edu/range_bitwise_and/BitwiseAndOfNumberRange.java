4ca2a21849762f0858d0c1bbc52c70101139d176
Ivan Veselovskii <ivan.veselovskii@syncron.com>
2024-05-04 14:31:39 +0200

    more samples

--- src/main/java/edu/range_bitwise_and/BitwiseAndOfNumberRange.java
+++ src/main/java/edu/range_bitwise_and/BitwiseAndOfNumberRange.java
@@ -1,5 +1,7 @@
 package edu.range_bitwise_and;
 
+import static edu.common.MathUtils.maxContainedPowerOf2;
+
 /**
  Given two integers left and right that represent the range [left, right], return the
  bitwise AND of all numbers in this range, inclusive.
@@ -46,14 +48,5 @@ public class BitwiseAndOfNumberRange {
 
         // 3. And AND-ed with the high number:
         return result & right;
-    }
-
-    public static int maxContainedPowerOf2(int x) {
-        int cnt = -1;
-        while (x != 0) {
-            x >>>= 1;
-            cnt++;
-        }
-        return cnt;
     }
 }
